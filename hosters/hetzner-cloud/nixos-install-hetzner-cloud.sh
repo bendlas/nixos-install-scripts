4d8b12170dc4fa6252289cabc376ecc8615510f0
Herwig Hochleitner <herwig@bendlas.net>
2021-08-11 03:42:05 +0200

    bendlas adaptions

--- hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
+++ hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
@@ -41,13 +41,18 @@ set -e
 # disk on first boot. In case the NixOS live ISO is booted immediately on
 # first powerup, that does not happen. Thus we need to grow the partition
 # by deleting and re-creating it.
+sgdisk -n 12:-1G:0
 sgdisk -d 1 /dev/sda
 sgdisk -N 1 /dev/sda
 partprobe /dev/sda
 
 mkfs.ext4 -F /dev/sda1 # wipes all data!
+mkswap /dev/sda12
 
 mount /dev/sda1 /mnt
+mkdir -p /mnt/boot/efi
+mount /dev/sda15 /mnt/boot/efi
+swapon /dev/sda12
 
 nixos-generate-config --root /mnt
 
@@ -65,8 +70,7 @@ echo '
   services.openssh.enable = true;
 
   users.users.root.openssh.authorizedKeys.keys = [
-    # Replace this by your SSH pubkey!
-    "ssh-rsa AAAAAAAAAAA..."
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC70QgW3EnX781iFOlojPnwST1CiMZaWdQEJNgSbsvaEFeHwFNDr9Ma2kTzjFQnpLKfb7eAr7BsUX3uSJjFq6MDTfgynCSXtgOxaahTfoVFFvJdGZPtXU09k7xSW043A7Ziwi8iPM0EFKUb85W6v4S1VACpjD57SEs4enUsyrXO8XVBDpqRQLdPDXjyNqzZ0zafbs22bDYDUmgr3UTItSzrGG7fzPyP3D2cJ1HKptQNUBRwjMvduG5by+ONxtuNJ7XGtQfFOyLJl4QFCWCSNwVEzv0CqAfrbq3XmqsAMXZJeMNo0OG/XpgQT2W4oP0QcyW9hHvxe6S34DjXDCaN8SreTJqq/8n3gQIj2/bkW9gGOHceZ98BDVXAeVXQj4opd3qF1V3DkP7NhUZEpgqHZglpkmcZqiufpdJbhnbjjIAUPN9c2dpEKWiR+UTR0hUedERDEGge6caM0XpfKPDiFXQpNgMBhatRkp9iNwoCIbp1muzYZpiu8YFNFbZmRmXcW8o8b3/MoEWZZTvMcffk7Yk+K0lItLmR7wjAJVZXM/7CbP6bVECbYAGNaQ50ZlPgt1wAU9VoE9oV3U2bVmV6Vdic1w1LS3pCOT9DNOXkGvbxLxp/gwJVFwkHVBAHnSLCyRyNn3GL+rzPO0Mzej2Q9stPUExcoMBkm6e4pUatynHONw== herwig@lenix"
   ];
 }
 ' >> /mnt/etc/nixos/configuration.nix
